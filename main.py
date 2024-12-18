944b1952e37af595f0631bcbb2b4fa69348cc8eb
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-12-18 20:30:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-from scripts.bitrateanalysis import BitrateAnalysis
+from scripts.chunkgeneralanalysis import BitrateChunkGeneralAnalysis, TimeChunkGeneralAnalysis, QualityChunkGeneralAnalysis, GetTilesChunkGeneralAnalysis
+from scripts.config import Config
 
-BitrateAnalysis()
+
+class App:
+    def __init__(self):
+        """
+        metric = 'bitrate' | 'time' | 'chunk_quality' | 'get_tiles
+        """
+        config = Config()
+        # ChunkGeneralAnalysis
+        # BitrateChunkGeneralAnalysis(config)
+        # TimeChunkGeneralAnalysis(config)
+        QualityChunkGeneralAnalysis(config)
+        # GetTilesChunkGeneralAnalysis(config)
+
+        # print('ByQuality')
+        # ByQuality(config, 'bitrate')
+        # print('ByTiling')
+        # ByTiling(config, 'bitrate')
+        # print('ByTilingByQuality')
+        # ByTilingByQuality(config, 'bitrate')
+
+
+App()
