6e1755956978ba40b5f154eabdfa569e54c7ba87
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-01-29 15:27:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
-from scripts.chunkgeneralanalysis import QualityChunkGeneralAnalysis, BitrateChunkGeneralAnalysis, TimeChunkGeneralAnalysis, GetTilesChunkGeneralAnalysis
+import argparse
+import json
+
+from scripts.app import ByTilingByQuality, ByTiling, ByQuality
+from scripts.chunkgeneralanalysis import (QualityChunkGeneralAnalysis,
+                                          BitrateChunkGeneralAnalysis,
+                                          TimeChunkGeneralAnalysis,
+                                          GetTilesChunkGeneralAnalysis)
 from scripts.config import Config
 from scripts.fix_database import FixDatabase
 from scripts.tilingqualitygeneralanalysis import BitrateTilingQualityGeneralAnalysis
 
+config = Config()
+
+workers = {
+    1: BitrateChunkGeneralAnalysis,
+    2: TimeChunkGeneralAnalysis,
+    3: QualityChunkGeneralAnalysis,
+    4: GetTilesChunkGeneralAnalysis,
+}
+
 
 class App:
-    def __init__(self):
+    def __init__(self, worker):
         """
         metric = 'bitrate' | 'time' | 'chunk_quality' | 'get_tiles
         """
-        config = Config()
-        # ChunkGeneralAnalysis
-        # BitrateChunkGeneralAnalysis(config)
-        # TimeChunkGeneralAnalysis(config)
-        QualityChunkGeneralAnalysis(config)
-        # GetTilesChunkGeneralAnalysis(config)
-
-        # print('ByTilingByQuality')
-        # BitrateTilingQualityGeneralAnalysis(config)
+        worker = workers[worker]
+        print(worker.__class__.__name__)
+        worker(config)
 
 
-        # ByQuality(config, 'bitrate')
-        # print('ByTiling')
-        # ByTiling(config, 'bitrate')
-        # print('ByTilingByQuality')
-        # ByTilingByQuality(config, 'bitrate')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description=json.dumps(workers, indent=4))
 
-        # config = Config()
-        # FixDatabase(config)
+    parser.add_argument('worker', type=int, metavar='WORKER',
+                        help=f'A worker name.')
 
-
-App()
+    args = parser.parse_args()
+    App(args.worker)
+    print(f'\nThe end.')
