9b56e093b2b9a5e992805d6f62584677ea160798
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-01-29 15:53:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,44 @@
 import argparse
 import json
 
+from scripts.config import Config
+
 from scripts.app import ByTilingByQuality, ByTiling, ByQuality
 from scripts.chunkgeneralanalysis import (QualityChunkGeneralAnalysis,
                                           BitrateChunkGeneralAnalysis,
                                           TimeChunkGeneralAnalysis,
                                           GetTilesChunkGeneralAnalysis)
-from scripts.config import Config
 from scripts.fix_database import FixDatabase
 from scripts.tilingqualitygeneralanalysis import BitrateTilingQualityGeneralAnalysis
 
 config = Config()
 
 workers = {
-    1: BitrateChunkGeneralAnalysis,
-    2: TimeChunkGeneralAnalysis,
-    3: QualityChunkGeneralAnalysis,
-    4: GetTilesChunkGeneralAnalysis,
+    1: BitrateChunkGeneralAnalysis.__name__,
+    2: TimeChunkGeneralAnalysis.__name__,
+    3: QualityChunkGeneralAnalysis.__name__,
+    4: GetTilesChunkGeneralAnalysis.__name__,
+    5: BitrateTilingQualityGeneralAnalysis.__name__,
+    6: FixDatabase.__name__,
+    7: ByTilingByQuality.__name__,
+    8: ByTiling.__name__,
+    9: ByQuality.__name__
 }
 
 
-class App:
-    def __init__(self, worker):
-        """
-        metric = 'bitrate' | 'time' | 'chunk_quality' | 'get_tiles
-        """
-        worker = workers[worker]
-        print(worker.__class__.__name__)
-        worker(config)
-
-
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     description=json.dumps(workers, indent=4))
+                                     description='WORKERS = ' + json.dumps(workers, indent=4))
 
     parser.add_argument('worker', type=int, metavar='WORKER',
                         help=f'A worker name.')
+    worker_id = parser.parse_args().worker
+    worker_str = workers[worker_id]
+    worker_class = globals()[worker_str]
 
-    args = parser.parse_args()
-    App(args.worker)
-    print(f'\nThe end.')
+    print(worker_str)
+    worker_class(config)
+
+
+if __name__ == '__main__':
+    main()
