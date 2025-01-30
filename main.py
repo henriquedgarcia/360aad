8bbf35db25740af8dbde476e6b436e6007757964
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-01-29 21:36:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,22 @@
 import argparse
 import json
 
-from scripts.app import ByTilingByQuality, ByTiling, ByQuality
-from scripts.chunkgeneralanalysis import (QualityChunkGeneralAnalysis,
-                                          BitrateChunkGeneralAnalysis,
-                                          TimeChunkGeneralAnalysis,
-                                          GetTilesChunkGeneralAnalysis)
+from scripts.chunkgeneralanalysis import (QualityChunkGeneralAnalysis, BitrateChunkGeneralAnalysis, TimeChunkGeneralAnalysis, )
+from scripts.chunktilingqualitygeneralanalysis import (BitrateTilingQualityGeneralAnalysis, QualityTilingQualityGeneralAnalysis, TimeTilingQualityGeneralAnalysis)
 from scripts.config import Config
-from scripts.fix_database import FixDatabase
-from scripts.chunktilingqualitygeneralanalysis import BitrateTilingQualityGeneralAnalysis
 
 config = Config()
 
-workers = {
-    1: BitrateChunkGeneralAnalysis.__name__,
-    2: TimeChunkGeneralAnalysis.__name__,
-    3: QualityChunkGeneralAnalysis.__name__,
-    4: GetTilesChunkGeneralAnalysis.__name__,
-    5: BitrateTilingQualityGeneralAnalysis.__name__,
-    6: FixDatabase.__name__,
-    7: ByTilingByQuality.__name__,
-    8: ByTiling.__name__,
-    9: ByQuality.__name__
-}
+workers = {1: BitrateChunkGeneralAnalysis.__name__, 2: TimeChunkGeneralAnalysis.__name__, 3: QualityChunkGeneralAnalysis.__name__, 4: BitrateTilingQualityGeneralAnalysis.__name__,
+    5: TimeTilingQualityGeneralAnalysis.__name__, 6: QualityTilingQualityGeneralAnalysis.__name__, }
 
 help_txt = 'WORKERS = ' + json.dumps(workers, indent=4)
 
 
 def main():
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     description=help_txt)
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=help_txt)
 
-    parser.add_argument('worker', type=int, metavar='WORKER', nargs='?',
-                        default=None, help=f'A worker name.')
+    parser.add_argument('worker', type=int, metavar='WORKER', nargs='?', default=None, help=f'A worker name.')
 
     worker_id = parser.parse_args().worker
     while True:
