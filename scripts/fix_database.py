8b54ad4e0059933e9101a2247430169607d2c4f1
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-02-08 18:28:04 -0300

    Update fix_database.py

--- scripts/fix_database.py
+++ scripts/fix_database.py
@@ -1,68 +1,110 @@
 from pathlib import Path
 
+import pandas as pd
+
 from scripts.analysisbase import AnalysisBase
-from scripts.database import database_factory
-from scripts.progressbar import ProgressBar
-from scripts.utils import AutoDict, set_nested_value, save_json
+from scripts.bucket import Bucket
 
 
 class FixDatabase(AnalysisBase):
     """
     Database[name][projection][tiling][tile][quality][chunk]
     """
-    metric = 'bitrate'
-    categories = {'bitrate': ['dash_mpd', 'dash_init', 'dash_m4s'],
-                  'time': ['dectime', 'dectime_avg', 'dectime_med', 'dectime_std'],
-                  'chunk_quality': ['ssim', 'mse', 's-mse', 'ws-mse']}
+
+    def setup(self):
+        pass
+
+    def make_bucket(self) -> Bucket:
+        pass
+
+    def make_stats(self):
+        pass
+
+    def plots(self):
+        pass
+
+    metrics = {'bitrate': ['dash_m4s'],
+               'time': ['dectime_avg'],
+               'chunk_quality': ['ssim', 'mse', 's-mse', 'ws-mse']}
+    dataset_structute = {'bitrate': {'dash_mpd': ['name', 'projection', 'tiling', 'tile'],
+                                     'dash_init': ['name', 'projection', 'tiling', 'tile', 'quality'],
+                                     'dash_m4s': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk']},
+                         'time': {'dectime': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk'],
+                                  'dectime_avg': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk'],
+                                  'dectime_med': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk'],
+                                  'dectime_std': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk'],
+                                  },
+                         'chunk_quality': {'ssim': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk'],
+                                           'mse': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk'],
+                                           's-mse': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk'],
+                                           'ws-mse': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk'],
+                                           }
+                         }
+    database_keys = {'dash_m4s': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk'],
+                     'dectime_avg': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk'],
+                     'ssim': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk'],
+                     'mse': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk'],
+                     's-mse': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk'],
+                     'ws-mse': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk'],
+                     }
+
     databases: dict
+    new_names = {'dash_m4s': 'bitrate',
+                 'dectime_avg': 'dectime',
+                 'ssim': 'ssim',
+                 'mse': 'mse',
+                 's-mse': 's-mse',
+                 'ws-ms': 'ws-ms',
+                 }
+    categories = ''
+    bucket_keys_name = ['']
+    bucket = {}
 
-    def main(self):
-        self.fix()
+    def __init__(self, config):
+        print(f'{self.__class__.__name__} initializing...')
+        self.config = config
 
-    def fix(self):
-        self.databases = {}
-        for self.metric in self.categories:
-            self.databases[self.metric] = database_factory(self.metric, self.config)
+        new_name = Path(f'dataset/global_dataset.h5')
 
-        print(f'Fix {self.metric}.')
-        self.ui = ProgressBar(28 * 181, f'Fix {self.metric}.')
-        for self.name in self.name_list:
-            new_name = Path(f'dataset/bitrate_time_quality/dataset_{self.name}.json')
-            if new_name.exists():
-                continue
+        if new_name.exists():
+            return
 
-            dataset = AutoDict()
-            for self.metric in self.categories:
-                self.databases[self.metric].load(self.database_json)
+        for self.metric in self.metrics:
+            for self.category in self.metrics[self.metric]:
+                df_name = self.new_names[self.category]
+                try:
+                    pd.read_hdf(new_name, key=df_name)
+                    continue
+                except (FileNotFoundError, KeyError):
+                    pass
 
-            for self.projection in self.projection_list:
-                for self.tiling in self.tiling_list:
-                    for self.tile in self.tile_list:
-                        self.ui.update(f'{self}')
-                        for self.quality in self.quality_list:
-                            for self.chunk in self.chunk_list:
-                                self.category = 'dash_m4s'
-                                dash_m4s = self.databases['bitrate'].get_value()
-                                self.category = 'dectime_avg'
-                                dectime_avg = self.databases['time'].get_value()
-                                self.category = 'ssim'
-                                ssim = self.databases['chunk_quality'].get_value()
-                                self.category = 'mse'
-                                mse = self.databases['chunk_quality'].get_value()
-                                self.category = 's-mse'
-                                s_mse = self.databases['chunk_quality'].get_value()
-                                self.category = 'ws-mse'
-                                ws_mse = self.databases['chunk_quality'].get_value()
-                                value = {'bitrate': dash_m4s,
-                                         'dectime': dectime_avg,
-                                         'ssim': ssim,
-                                         'mse': mse,
-                                         's_mse': s_mse,
-                                         'ws_mse': ws_mse}
+                index = pd.MultiIndex.from_tuples([], names=['name', 'projection', 'tiling', 'tile', 'quality', 'chunk'])
+                df = pd.DataFrame(columns=['value'], index=index)
 
-                                keys = [self.name, self.projection,
-                                        self.tiling, self.tile, self.quality, self.chunk]
-                                set_nested_value(dataset, keys, value)
-                        self.quality = self.chunk = None
+                for self.name in self.name_list:
+                    self.load_database()
+                    total = (181
+                             * len(self.quality_list)
+                             # * len(self.chunk_list)
+                             # * len(self.metrics[self.metric])
+                             )
+                    self.start_ui(total, f'\t{self.category}_{self.name}')
 
-            save_json(dataset, new_name)
+                    for self.projection in self.projection_list:
+                        for self.tiling in self.tiling_list:
+                            for self.tile in self.tile_list:
+                                for self.quality in self.quality_list:
+                                    self.update_ui(f'{self.tiling}/{self.tile}_qp{self.quality}')
+                                    for self.chunk in self.chunk_list:
+                                        value = self.get_dataset_value(self.category)
+
+                                        df.loc[(self.name,
+                                                self.projection,
+                                                self.tiling,
+                                                self.tile,
+                                                self.quality,
+                                                self.chunk,
+                                                )] = value
+                print('Saving new database...')
+                mode = 'a' if new_name.exists() else 'w'
+                df.to_hdf(new_name, key=df_name, mode=mode)
