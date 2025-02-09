f73e6ca78fce5bde3e01b50ed94d5b0233f8443d
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-02-09 14:53:42 -0300

    Update analysisbase.py

--- scripts/analysisbase.py
+++ scripts/analysisbase.py
@@ -5,10 +5,9 @@ from pathlib import Path
 
 import pandas as pd
 
-from scripts.bucket import Bucket
 from scripts.config import ConfigIf
 from scripts.progressbar import ProgressBar
-from scripts.utils import load_json, get_nested_value, set_nested_value, get_bucket_value, set_bucket_value
+from scripts.utils import get_nested_value, get_bucket_value, set_bucket_value, load_pickle
 
 
 class AnalysisPaths(ConfigIf):
@@ -92,41 +91,49 @@ class AnalysisPaths(ConfigIf):
 
     @property
     def bucket_pickle(self):
-        cat = '-'.join(self.categories)
-        keys = '_'.join(self.bucket_keys_name)
-        return self.bucket_workfolder / f'{self.metric}_[{cat}]_{keys}.pickle'
-
-    @property
-    def database_json(self):
-        database_path = Path(f'dataset/{self.metric}')
-        database_json_path = database_path / f'{self.metric}_{self.config.name}.json'
-        if self.metric == 'get_tiles':
-            database_json_path_stem = database_json_path.stem + '_fov110x90'
-            database_json_path = database_json_path.with_stem(database_json_path_stem)
-        return database_json_path
+        return self.bucket_workfolder / f'bucket_{self.metric}.pickle'
 
 
 class AnalysisBase(AnalysisPaths, ABC):
+    dataset_structure = {'dash_mpd': {'path': f'dataset/df_dash_mpd.pickle',
+                                      'keys': ['name', 'projection', 'tiling', 'tile']
+                                      },
+                         'dash_init': {'path': f'dataset/df_dash_init.pickle',
+                                       'keys': ['name', 'projection', 'tiling', 'tile', 'quality']
+                                       },
+                         'dash_m4s': {'path': f'dataset/df_dash_m4s.pickle',
+                                      'keys': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk']
+                                      },
+                         'dectime_avg': {'path': f'dataset/df_dectime_avg.pickle',
+                                         'keys': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk']
+                                         },
+                         'dectime_std': {'path': f'dataset/df_dectime_std.pickle',
+                                         'keys': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk']
+                                         },
+                         'ssim': {'path': f'dataset/df_ssim.pickle',
+                                  'keys': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk']
+                                  },
+                         'mse': {'path': f'dataset/df_mse.pickle',
+                                 'keys': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk']
+                                 },
+                         's-mse': {'path': f'dataset/df_s-mse.pickle',
+                                   'keys': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk']
+                                   },
+                         'ws-mse': {'path': f'dataset/df_ws-mse.pickle',
+                                    'keys': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk']
+                                    },
+                         }
+
     def __init__(self, config):
         print(f'{self.__class__.__name__} initializing...')
         self.config = config
-
         self.setup()
-        self._make_bucket()
-        self._make_stats()
-        self.plots()
 
-    def _make_bucket(self):
-        try:
-            self.load_bucket()
-        except FileNotFoundError:
-            self.make_bucket()
-            self.save_bucket()
-
-    def _make_stats(self):
         if not self.stats_csv.exists():
             self.make_stats()
             self.save_stats()
+
+        self.plots()
 
     def save_stats(self):
         self.stats_df: pd.DataFrame = pd.DataFrame(self.stats_defaultdict)
@@ -134,10 +141,6 @@ class AnalysisBase(AnalysisPaths, ABC):
 
     @abstractmethod
     def setup(self):
-        ...
-
-    @abstractmethod
-    def make_bucket(self) -> Bucket:
         ...
 
     @abstractmethod
@@ -149,11 +152,13 @@ class AnalysisBase(AnalysisPaths, ABC):
         ...
 
     def load_database(self):
-        print(f'{self.__class__.__name__} loading {self.name} database...')
-        self.database = load_json(self.database_json)
+        print(f'{self.__class__.__name__} loading {self.metric} database...')
+        filename = self.dataset_structure[self.metric]['path']
+        self.database = load_pickle(filename)
 
-    def get_dataset_value(self, category):
-        database_keys = [getattr(self, key) for key in self.database_keys[category]] + [category]
+    def get_database_value(self):
+        keys_name = self.dataset_structure[self.metric]['keys']
+        database_keys = [getattr(self, key) for key in keys_name]
         value = get_nested_value(self.database, database_keys)
         return value
 
@@ -164,11 +169,10 @@ class AnalysisBase(AnalysisPaths, ABC):
 
         # self.bucket = pickle.loads(self.bucket_pickle.read_bytes())
 
-    # noinspection PyTypeChecker
     def save_bucket(self):
         with open(self.bucket_pickle, 'wb') as f:
+            # noinspection PyTypeChecker
             pickle.dump(self.bucket, f)
-        # self.bucket_pickle.write_bytes(pickle.dumps(self.bucket))
 
     def get_bucket_keys(self, cat):
         bucket_keys = [cat] + [getattr(self, key) for key in self.bucket_keys_name]
