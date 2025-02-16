40c2c11106410b5e276d481deec52836d8725718
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-02-16 18:47:25 -0300

    Update serie_analysis_tiling_quality_chunk_frame.py

--- scripts/serie_analysis_tiling_quality_chunk_frame.py
+++ scripts/serie_analysis_tiling_quality_chunk_frame.py
@@ -1,7 +1,7 @@
 import os
 from collections import defaultdict
 
-import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
 
 from scripts.analysisbase import AnalysisBase
@@ -16,31 +16,63 @@ class SerieAnalysisTilingQualityChunkFrame(AnalysisBase):
         self.projection = 'cmp'
         del self.dataset_structure['dash_mpd']
         del self.dataset_structure['dash_init']
-        del self.dataset_structure['dectime_std']
+        # del self.dataset_structure['dectime_std']
 
     def make_stats(self):
-        return
-
-    def get_data(self):
-        new_db = self.database.xs((self.tiling, self.quality), level=('tiling', 'quality'))
-        chunk_data = new_db.groupby(['chunk']).mean()
-        data = list(chunk_data['value'])
-        return data
-
-    def plots(self):
-        self.make_plot_quality_tiling()
-        self.make_boxplot_tiling_quality()
-
-    def make_plot_quality_tiling(self):
-        print(f'make_boxplot_quality_tiling.')
+        print(f'make_stats.')
         for self.metric in self.dataset_structure:
             self.load_database()
-            self.database = self.database.groupby(['name', 'projection', 'tiling', 'quality', 'chunk']).sum()
+            for self.tiling in self.tiling_list:
+                for self.quality in self.quality_list:
+                    serie: pd.Series = self.get_chunk_data()
+                    self.stats_defaultdict['Metric'].append(self.metric)
+                    self.stats_defaultdict['Tiling'].append(self.tiling)
+                    self.stats_defaultdict['Quality'].append(self.quality)
+                    self.stats_defaultdict['n_arquivos'].append(len(serie))
+                    self.stats_defaultdict['Média'].append(serie.mean())
+                    self.stats_defaultdict['Desvio Padrão'].append(serie.std())
+                    self.stats_defaultdict['Mínimo'].append(serie.quantile(0.00))
+                    self.stats_defaultdict['1º Quartil'].append(serie.quantile(0.25))
+                    self.stats_defaultdict['Mediana'].append(serie.quantile(0.50))
+                    self.stats_defaultdict['3º Quartil'].append(serie.quantile(0.75))
+                    self.stats_defaultdict['Máximo'].append(serie.quantile(1.00))
 
-            boxplot_path_quality = self.boxplot_folder / f'boxplot_{self.metric}_quality.pdf'
-            if boxplot_path_quality.exists():
-                print(f'\t{boxplot_path_quality} exists.')
+    def get_chunk_data(self):
+        chunk_data = self.database.loc[(self.tiling, self.quality)]['value']
+        return chunk_data
+
+    def load_database(self):
+        super().load_database()
+        self.database = self.database.groupby(['tiling', 'quality', 'tile', 'chunk']).mean()
+        index_l3_int = self.database.index.levels[3].astype(int)
+        self.database.index = self.database.index.set_levels(index_l3_int, level=3)
+        self.database.sort_index(inplace=True)
+
+        if self.metric in ['dash_m4s', 'dectime_avg']:
+            self.database = self.database.groupby(['tiling', 'quality', 'chunk']).sum()
+        else:
+            self.database = self.database.groupby(['tiling', 'quality', 'chunk']).mean()
+
+    def plots(self):
+        self.make_plot_quality_tiling_frame()
+        self.make_plot_tiling_quality_frame()
+        self.make_boxplot_quality_tiling_frame()
+        self.make_boxplot_tiling_quality_frame()
+        self.make_violinplot_quality_tiling_frame()
+        self.make_violinplot_tiling_quality_frame()
+
+    def make_plot_quality_tiling_frame(self):
+        print(f'make_boxplot_quality_tiling.')
+        # By metric ['dash_m4s', 'dectime_avg', 'ssim','mse', 's-mse', 'ws-mse']
+        for self.metric in self.dataset_structure:
+            # Check files
+            boxplot_path = self.series_plot_folder / f'plot_{self.metric}_quality.pdf'
+            if boxplot_path.exists():
+                print(f'\t{boxplot_path} exists.')
                 continue
+
+            # Load Database
+            self.load_database()
 
             fig = plt.figure(figsize=(6, 7.5), layout='tight')
             fig.suptitle(f'{self.metric}')
@@ -50,28 +82,33 @@ class SerieAnalysisTilingQualityChunkFrame(AnalysisBase):
 
                 ax: plt.Axes = fig.add_subplot(3, 2, n)
                 for self.tiling in self.tiling_list:
-                    bucket = self.get_data()
-                    ax.plot(bucket, label=f'{self.tiling}')
+                    serie = self.get_chunk_data()
+                    ax.plot(serie, label=f'{self.tiling}')
 
                 ax.set_title(f'qp{self.quality}')
                 ax.set_xlabel(f'Chunk')
                 ax.legend(loc='upper right')
                 ax.set_ylabel(self.dataset_structure[self.metric]['quantity'])
+
                 if self.metric == 'dash_m4s':
                     ax.ticklabel_format(axis='y', style='scientific',
                                         scilimits=(6, 6))
-            fig.savefig(boxplot_path_quality)
+
+            fig.savefig(boxplot_path)
             fig.clf()
 
-    def make_boxplot_tiling_quality(self):
+    def make_plot_tiling_quality_frame(self):
         print(f'make_boxplot_tiling_quality.')
+        # By metric ['dash_m4s', 'dectime_avg', 'ssim','mse', 's-mse', 'ws-mse']
         for self.metric in self.dataset_structure:
-            self.load_database()
-
-            boxplot_path = self.boxplot_folder / f'boxplot_{self.metric}_tiling.pdf'
+            # Check files
+            boxplot_path = self.series_plot_folder / f'plot_{self.metric}_tiling.pdf'
             if boxplot_path.exists():
                 print(f'\t{boxplot_path} exists.')
                 continue
+
+            # Load Database
+            self.load_database()
 
             fig = plt.figure(figsize=(6, 7.5), layout='tight')
             fig.suptitle(f'{self.metric}')
@@ -81,17 +118,154 @@ class SerieAnalysisTilingQualityChunkFrame(AnalysisBase):
 
                 ax: plt.Axes = fig.add_subplot(3, 2, n)
                 for self.quality in self.quality_list:
-                    bucket = self.get_data()
-                    ax.plot(bucket, label=f'qp{self.quality}')
+                    serie = self.get_chunk_data()
+                    ax.plot(serie, label=f'qp{self.quality}')
 
                 ax.set_title(f'{self.tiling}')
                 ax.set_xlabel(f'Chunk')
-                ax.legend()
+                ax.legend(loc='upper right')
                 ax.set_ylabel(self.dataset_structure[self.metric]['quantity'])
+
                 if self.metric == 'dash_m4s':
                     ax.ticklabel_format(axis='y', style='scientific',
                                         scilimits=(6, 6))
 
+            fig.savefig(boxplot_path)
+            fig.clf()
+
+    def make_boxplot_quality_tiling_frame(self):
+        print(f'make_boxplot_quality_tiling.')
+        # By metric ['dash_m4s', 'dectime_avg', 'ssim','mse', 's-mse', 'ws-mse']
+        for self.metric in self.dataset_structure:
+            # Check files
+            boxplot_path = self.boxplot_folder / f'boxplot_{self.metric}_quality.pdf'
+            if boxplot_path.exists():
+                print(f'\t{boxplot_path} exists.')
+                continue
+
+            # Load Database
+            self.load_database()
+
+            fig = plt.figure(figsize=(6, 7.5), layout='tight')
+            fig.suptitle(f'{self.metric}')
+
+            for n, self.quality in enumerate(self.quality_list, 1):
+                print(f'Plot qp{self.quality}')
+                ax: plt.Axes = fig.add_subplot(3, 2, n)
+
+                serie_list = [self.get_chunk_data() for self.tiling in self.tiling_list]
+                ax.boxplot(serie_list, tick_labels=list(self.tiling_list))
+
+                ax.set_title(f'qp{self.quality}')
+                ax.set_xlabel(f'Tiling')
+                ax.set_ylabel(self.dataset_structure[self.metric]['quantity'])
+
+                if self.metric == 'dash_m4s':
+                    ax.ticklabel_format(axis='y', style='scientific',
+                                        scilimits=(6, 6))
+            fig.savefig(boxplot_path)
+            fig.clf()
+
+    def make_boxplot_tiling_quality_frame(self):
+        print(f'make_boxplot_tiling_quality.')
+        # By metric ['dash_m4s', 'dectime_avg', 'ssim','mse', 's-mse', 'ws-mse']
+        for self.metric in self.dataset_structure:
+            # Check files
+            boxplot_path = self.boxplot_folder / f'boxplot_{self.metric}_tiling.pdf'
+            if boxplot_path.exists():
+                print(f'\t{boxplot_path} exists.')
+                continue
+
+            # Load Database
+            self.load_database()
+
+            fig = plt.figure(figsize=(6, 7.5), layout='tight')
+            fig.suptitle(f'{self.metric}')
+
+            for n, self.tiling in enumerate(self.tiling_list, 1):
+                print(f'Plot {self.tiling}')
+                ax: plt.Axes = fig.add_subplot(3, 2, n)
+
+                serie_list = [self.get_chunk_data() for self.quality in self.quality_list]
+                ax.boxplot(serie_list, tick_labels=list(self.quality_list))
+
+                ax.set_title(f'{self.tiling}')
+                ax.set_xlabel(f'Quality')
+                ax.set_ylabel(self.dataset_structure[self.metric]['quantity'])
+
+                if self.metric == 'dash_m4s':
+                    ax.ticklabel_format(axis='y', style='scientific',
+                                        scilimits=(6, 6))
+            fig.savefig(boxplot_path)
+            fig.clf()
+
+    def make_violinplot_quality_tiling_frame(self):
+        print(f'make_violinplot_quality_tiling_frame.')
+        # By metric ['dash_m4s', 'dectime_avg', 'ssim','mse', 's-mse', 'ws-mse']
+        for self.metric in self.dataset_structure:
+            # Check files
+            boxplot_path = self.violinplot_folder / f'violinplot_{self.metric}_quality.pdf'
+            if boxplot_path.exists():
+                print(f'\t{boxplot_path} exists.')
+                continue
+
+            # Load Database
+            self.load_database()
+
+            fig = plt.figure(figsize=(6, 7.5), layout='tight')
+            fig.suptitle(f'{self.metric}')
+
+            for n, self.quality in enumerate(self.quality_list, 1):
+                print(f'Plot qp{self.quality}')
+                ax: plt.Axes = fig.add_subplot(3, 2, n)
+
+                serie_list = [self.get_chunk_data() for self.tiling in self.tiling_list]
+                ax.violinplot(serie_list, showmeans=False, showmedians=True)
+                ax.set_xticks(list(range(1, len(self.tiling_list) + 1)),
+                              list(self.tiling_list))
+
+                ax.set_title(f'qp{self.quality}')
+                ax.set_xlabel(f'Tiling')
+                ax.set_ylabel(self.dataset_structure[self.metric]['quantity'])
+
+                if self.metric == 'dash_m4s':
+                    ax.ticklabel_format(axis='y', style='scientific',
+                                        scilimits=(6, 6))
+            fig.savefig(boxplot_path)
+            fig.clf()
+
+    def make_violinplot_tiling_quality_frame(self):
+        print(f'make_boxplot_tiling_quality.')
+        # By metric ['dash_m4s', 'dectime_avg', 'ssim','mse', 's-mse', 'ws-mse']
+        for self.metric in self.dataset_structure:
+            # Check files
+            boxplot_path = self.violinplot_folder / f'violinplot_{self.metric}_tiling.pdf'
+            if boxplot_path.exists():
+                print(f'\t{boxplot_path} exists.')
+                continue
+
+            # Load Database
+            self.load_database()
+
+            fig = plt.figure(figsize=(6, 7.5), layout='tight')
+            fig.suptitle(f'{self.metric}')
+
+            for n, self.tiling in enumerate(self.tiling_list, 1):
+                print(f'Plot {self.tiling}')
+                ax: plt.Axes = fig.add_subplot(3, 2, n)
+
+                serie_list = [self.get_chunk_data() for self.quality in self.quality_list]
+                ax.violinplot(serie_list, showmeans=False, showmedians=True)
+                ax.set_xticks(list(range(1, len(self.quality_list) + 1)),
+                              list(self.quality_list))
+
+                ax.set_title(f'{self.tiling}')
+                ax.set_xlabel(f'Quality')
+                ax.set_ylabel(self.dataset_structure[self.metric]['quantity'])
+
+                if self.metric == 'dash_m4s':
+                    ax.ticklabel_format(axis='y', style='scientific',
+                                        scilimits=(6, 6))
             fig.savefig(boxplot_path)
             fig.clf()
 
