110706a889040384de7a3fd51ce7800f0c20f0f3
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-03-02 18:31:33 -0300

    Update fix_database.py

--- scripts/fix_database.py
+++ scripts/fix_database.py
@@ -7,44 +7,27 @@ import pandas as pd
 from scripts.analysisbase import AnalysisPaths
 from scripts.utils.config import Config
 from scripts.utils.progressbar import ProgressBar
-from scripts.utils.utils import dict_to_tuples, AutoDict, save_pickle, load_json, load_pickle
+from scripts.utils.utils import load_json
 
 
 class FixDatabase(AnalysisPaths):
-    dataset_structure = {'get_tiles': {'get_tiles': ['name', 'projection', 'tiling', 'tile', 'category', 'value', None, None],
-                                       },
-                         'bitrate': {'dash_mpd': ['name', 'projection', 'tiling', 'tile', 'category', 'value', None, None],
-                                     'dash_init': ['name', 'projection', 'tiling', 'tile', 'quality', 'category', 'value', None],
-                                     'dash_m4s': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'value']
-                                     },
-                         'time': {'dectime_avg': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'value'],
-                                  'dectime_std': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'value'],
-                                  },
-                         'chunk_quality': {'ssim': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'frame', 'value'],
-                                           'mse': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'frame', 'value'],
-                                           's-mse': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'frame', 'value'],
-                                           'ws-mse': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'frame', 'value'],
-                                           }
-                         }
-    metrics = list(dataset_structure)
-    databases: dict
-    new_names = {'get_tiles': 'get_tiles',
-
-                 'dash_mpd': 'dash_mpd',
-                 'dash_init': 'dash_init',
-                 'dash_m4s': 'bitrate',
-
-                 'dectime_avg': 'dectime',
-                 'dectime_std': 'dectime_std',
-
-                 'ssim': 'ssim',
-                 'mse': 'mse',
-                 's-mse': 's-mse',
-                 'ws-ms': 'ws-ms',
-                 }
-    categories = ''
-    bucket_keys_name = ['']
-    bucket = {}
+    dataset_structure = {
+        'get_tiles': {'get_tiles_chunk': ['name', 'projection', 'tiling', 'tile', 'category', 'value', None, None],
+                      'get_tiles_frame': ['name', 'projection', 'tiling', 'tile', 'category', 'value', None, None],
+                      },
+        'bitrate': {'dash_mpd': ['name', 'projection', 'tiling', 'tile', 'category', 'value', None, None],
+                    'dash_init': ['name', 'projection', 'tiling', 'tile', 'quality', 'category', 'value', None],
+                    'dash_m4s': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'value']
+                    },
+        'time': {'dectime_avg': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'value'],
+                 'dectime_std': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'value'],
+                 },
+        'chunk_quality': {'ssim': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'frame', 'value'],
+                          'mse': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'frame', 'value'],
+                          's-mse': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'frame', 'value'],
+                          'ws-mse': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'frame', 'value'],
+                          }
+    }
 
     @property
     def database_json(self):
@@ -54,43 +37,87 @@ class FixDatabase(AnalysisPaths):
     def __init__(self, config):
         print(f'{self.__class__.__name__} initializing...')
         self.config = config
+        self.projection = 'cmp'
         self.split_cat()
-        self.to_dataframe()
 
     def split_cat(self):
-        self.metric = 'get_tiles'
-        self.projection='cmp'
+
+        self.metric = 'chunk_quality'
         if work := True:
-            cat_db1 = AutoDict()
-            cat_db2 = AutoDict()
+            index_quality = pd.MultiIndex.from_tuples([], names=["name", "projection", "tiling", "tile", "quality", "chunk"])
+            df = pd.DataFrame(index=index_quality, columns=["ssim", "mse", "smse", "wsmse"])
+
             for self.name in self.name_list:
-                database_json = self.database_json.with_stem(self.database_json.stem + '_fov110x90')
-                self.database = load_json(database_json)
+                self.database = load_json(self.database_json)
+
+                for self.projection in self.projection_list:
+                    for self.tiling in self.tiling_list:
+                        for self.tile in self.tile_list:
+                            print(f'\r{self.name} {self.projection} {self.tiling}/{self.tile}', end='')
+
+                            for self.quality in self.quality_list:
+                                for self.chunk in self.chunk_list:
+                                    data_ssim = self.database[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk]['ssim']
+                                    data_mse = self.database[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk]['mse']
+                                    data_smse = self.database[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk]['s-mse']
+                                    data_wsmse = self.database[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk]['ws-mse']
+
+                                    df.loc[(self.name, self.projection, self.tiling, int(self.tile), int(self.quality), int(self.chunk))] = [
+                                        float(np.average(data_ssim)),
+                                        float(np.average(data_mse)),
+                                        float(np.average(data_smse)),
+                                        float(np.average(data_wsmse))
+                                    ]
+            df["ssim"].to_pickle(f'dataset/ssim.pickle')
+            df["mse"].to_pickle(f'dataset/mse.pickle')
+            df["smse"].to_pickle(f'dataset/s_mse.pickle')
+            df["wsmse"].to_pickle(f'dataset/ws_mse.pickle')
+
+        self.metric = 'get_tiles'
+        if work := True:
+            # chunk_data = []
+            # for self.name in self.name_list:
+            #     self.database = load_json(self.database_json.with_stem(self.database_json.stem + '_fov110x90'))
+            #
+            #     for self.tiling in self.tiling_list:
+            #         for i, self.user in enumerate(self.users_list):
+            #             print(f'\r{self.name} {self.projection} {self.tiling} user{i}', end='')
+            #
+            #             for self.chunk in self.chunk_list:
+            #                 chunk_tile_list = self.database[self.name][self.projection][self.tiling][self.user]['chunks'][self.chunk]
+            #                 chunk_tile_serie = pd.Series(chunk_tile_list).astype(int)
+            #                 register = (self.name, self.projection, self.tiling, int(self.user),
+            #                             int(self.chunk), chunk_tile_serie)
+            #                 chunk_data.append(register)
+            #
+            # columns = ["name", "projection", "tiling", "chunk", "user", "value"]
+            # df = pd.DataFrame(chunk_data, columns=columns).set_index(columns[:-1])
+            # df["value"].to_pickle(f'dataset/get_tiles_chunk.pickle')
+
+            frame_data = []
+            for self.name in self.name_list:
+                self.database = load_json(self.database_json.with_stem(self.database_json.stem + '_fov110x90'))
 
                 for self.tiling in self.tiling_list:
-                    for self.user in self.users_list:
-                        print(f'\r{self.name} {self.projection} {self.tiling}/frame', end='')
-                        for frame in range(1800):
-                            frame_tile_list = self.database[self.name][self.projection][self.tiling][self.user]['frames'][frame]
-                            cat_db1[int(self.user)][self.name][self.projection][self.tiling][frame] = frame_tile_list
-                        print(f'\r{self.name} {self.projection} {self.tiling}/chunk', end='')
-                        for self.chunk in self.chunk_list:
-                            chunk_tile_list = self.database[self.name][self.projection][self.tiling][self.user]['chunks'][self.chunk]
-                            cat_db2[int(self.user)][self.name][self.projection][self.tiling][int(self.chunk)] = chunk_tile_list
-            save_pickle(cat_db1, f'dataset/get_tiles_frame.pickle')
-            save_pickle(cat_db2, f'dataset/get_tiles_chunk.pickle')
+                    for i, self.user in enumerate(self.users_list):
+                        print(f'\r{self.name} {self.projection} {self.tiling} user{i}', end='')
+
+                        for self.frame in range(1800):
+                            frame_tile_list = self.database[self.name][self.projection][self.tiling][self.user]['frames'][self.frame]
+                            frame_tile_serie = pd.Series(frame_tile_list).astype(int)
+                            register = (self.name, self.projection, self.tiling, int(self.user),
+                                        self.frame, frame_tile_serie)
+                            frame_data.append(register)
+
+            columns = ["user", "name", "projection", "tiling", "frame", "value"]
+            index = ["user", "name", "projection", "tiling", "frame"]
+            df = pd.DataFrame(frame_data, columns=columns).set_index(index)
+            df["value"].to_pickle(f'dataset/get_tiles_frame.pickle')
 
         self.metric = 'bitrate'
         if work := False:
-            cat_db1 = AutoDict()
-            cat_db2 = AutoDict()
-            cat_db3 = AutoDict()
-            total = (len(self.name_list) *
-                     181 *
-                     len(self.quality_list) *
-                     len(self.chunk_list)
-                     )
-            self.start_ui(total, f'{self.metric}')
+            bitrate_data = []
+
             for self.name in self.name_list:
                 self.database = load_json(self.database_json)
 
@@ -98,26 +125,19 @@ class FixDatabase(AnalysisPaths):
                     for self.tiling in self.tiling_list:
                         for self.tile in self.tile_list:
                             print(f'\r{self.name} {self.projection} {self.tiling}/{self.tile}', end='')
-                            data = self.database[self.name][self.projection][self.tiling][self.tile]['dash_mpd']
-                            cat_db1[self.name][self.projection][self.tiling][self.tile] = int(data)
 
                             for self.quality in self.quality_list:
-                                data = self.database[self.name][self.projection][self.tiling][self.tile][self.quality]['dash_init']
-                                cat_db2[self.name][self.projection][self.tiling][self.tile][self.quality] = int(data)
-
                                 for self.chunk in self.chunk_list:
-                                    self.update_ui('')
                                     data = self.database[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk]['dash_m4s']
-                                    cat_db3[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk] = int(data)
+                                    bitrate_data.append((self.name, self.projection, self.tiling, int(self.tile), int(self.quality), int(self.chunk), int(data)))
 
-            save_pickle(cat_db1, f'dataset/dash_mpd.pickle')
-            save_pickle(cat_db2, f'dataset/dash_init.pickle')
-            save_pickle(cat_db3, f'dataset/dash_m4s.pickle')
+            columns = ["name", "projection", "tiling", "tile", "quality", "chunk", "value"]
+            df = pd.DataFrame(bitrate_data, columns=columns).set_index(columns[:-1])
+            df["value"].to_pickle(f'dataset/bitrate.pickle')
 
         self.metric = 'time'
         if work := False:
-            cat_db1 = AutoDict()
-            cat_db2 = AutoDict()
+            chunk_data = []
 
             for self.name in self.name_list:
                 self.database = load_json(self.database_json)
@@ -128,121 +148,22 @@ class FixDatabase(AnalysisPaths):
                             print(f'\r{self.name} {self.projection} {self.tiling}/{self.tile}', end='')
                             for self.quality in self.quality_list:
                                 for self.chunk in self.chunk_list:
-                                    data = self.database[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk]['dectime_avg']
-                                    cat_db1[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk] = float(data)
-                                    data = self.database[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk]['dectime_std']
-                                    cat_db2[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk] = float(data)
-
-            save_pickle(cat_db1, f'dataset/dectime_avg.pickle')
-            save_pickle(cat_db2, f'dataset/dectime_std.pickle')
-
-        self.metric = 'chunk_quality'
-        if work := False:
-            cat_db1 = AutoDict()
-            cat_db2 = AutoDict()
-            cat_db3 = AutoDict()
-            cat_db4 = AutoDict()
-
-            total = (len(self.name_list) *
-                     181 *
-                     len(self.quality_list) *
-                     len(self.chunk_list)
-                     )
-            self.start_ui(total, f'{self.metric}')
-            for self.name in self.name_list:
-                self.database = load_json(self.database_json)
-
-                for self.projection in self.projection_list:
-                    for self.tiling in self.tiling_list:
-                        for self.tile in self.tile_list:
-                            for self.quality in self.quality_list:
-                                for self.chunk in self.chunk_list:
-                                    self.update_ui('')
-                                    data = self.database[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk]['ssim']
-                                    cat_db1[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk] = float(np.average(data))
-                                    data = self.database[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk]['mse']
-                                    cat_db2[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk] = float(np.average(data))
-                                    data = self.database[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk]['s-mse']
-                                    cat_db3[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk] = float(np.average(data))
-                                    data = self.database[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk]['ws-mse']
-                                    cat_db4[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk] = float(np.average(data))
-
-            save_pickle(cat_db1, f'dataset/ssim.pickle')
-            save_pickle(cat_db2, f'dataset/mse.pickle')
-            save_pickle(cat_db3, f'dataset/s-mse.pickle')
-            save_pickle(cat_db4, f'dataset/ws-mse.pickle')
-
-    def to_hfs(self):
-        global_dataset_path = Path(f'dataset/global_dataset.h5')
-
-        for self.metric in self.metrics:
-            self.categories = self.dataset_structure[self.metric]
-
-            data_tuples = []
-
-            for self.category in self.categories:
-                # df_name = self.new_names[self.category]
-                df_name = self.category
-                try:
-                    pd.read_hdf(global_dataset_path, key=df_name)
-                    continue
-                except (FileNotFoundError, KeyError):
-                    pass
-
-                if not data_tuples:
-                    for self.name in self.name_list:
-                        self.database = load_json(self.database_json)
-                        data_tuples.extend(dict_to_tuples(self.database))
-
-                labels = self.dataset_structure[self.metric][self.category]
-                df = pd.DataFrame(data_tuples, columns=labels)
-                filtered_df = df[df['category'] == self.category]
-                del filtered_df['category']
-                if None in filtered_df.columns: del filtered_df[None]
-                new_index = [i for i in labels if i not in ['category', 'value', None]]
-                new_df = filtered_df.set_index(new_index)
-
-                print('Saving new database...')
-
-                t = int if self.metric == 'bitrate' else float
-                new_df['value'] = new_df['value'].astype(t)
-                new_df.to_hdf(global_dataset_path, key=df_name, mode='a')
-
-    def to_dataframe(self):
-        """
-        metric, categories, keys_orders
-        """
-        print(f'make_bucket')
-        for self.metric in self.dataset_structure:
-            print(f'make_{self.metric}_bucket')
-            if self.bucket_pickle.exists():
-                self.df = load_pickle(self.bucket_pickle)
-                return
-
-            filename = self.dataset_structure[self.metric]['path']
-            self.database = load_pickle(filename)
-
-            data_tuples = list(dict_to_tuples(self.database))
-            labels = self.dataset_structure[self.metric]['keys'] + ['value']
-            self.df = pd.DataFrame(data_tuples, columns=labels)
-
-            new_index = [i for i in labels[:-1]]
-            self.df.set_index(new_index, inplace=True)
-
-            print('Saving new database...')
-            t = int if self.metric == 'bitrate' else float
-            self.df['value'] = self.df['value'].astype(t)
-            save_pickle(self.df, self.bucket_pickle)
+                                    data_dectime = self.database[self.name][self.projection][self.tiling][self.tile][self.quality][self.chunk]['dectime_avg']
+                                    data_dectime = float(data_dectime)
+                                    register = (self.name, self.projection, self.tiling, int(self.tile),
+                                                int(self.quality), int(self.chunk), float(data_dectime))
+                                    chunk_data.append(register)
+            columns = ["name", "projection", "tiling", "tile", "quality", "chunk", "value"]
+            df = pd.DataFrame(chunk_data, columns=columns).set_index(columns[:-1])
+            df["value"].to_pickle(f'dataset/dectime.pickle')
 
     def start_ui(self, total, desc):
         self.ui = ProgressBar(total=total, desc=desc)
 
-    def update_ui(self, desc):
+    def update_ui(self, desc: str):
         self.ui.update(desc)
 
 
 if __name__ == '__main__':
     os.chdir('../')
-
-    config = Config()
-    FixDatabase(config)
+    FixDatabase(Config())
