3e89120dd6736cc62997576827b9710fb351db22
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-04-18 18:42:03 -0300

    Update analisys_siti.py

--- scripts/analisys_siti.py
+++ scripts/analisys_siti.py
@@ -1,5 +1,6 @@
 import os
 from collections import defaultdict
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
@@ -14,14 +15,19 @@ cor = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b']
 
 class SitiData:
     def __init__(self, filename, context: AnalysisBase):
-        self.level = ['name', 'projection', 'tiling', 'tile', 'quality']
+        self.level = ['name', 'projection', 'tiling', 'tile', 'quality', 'frame']
         self.columns = ['si', 'ti']
-        self.data: pd.DataFrame = load_pd_pickle(filename)
         self.context = context
+        self.data: pd.DataFrame = load_pd_pickle(Path(filename))
 
     def __getitem__(self, column) -> pd.Series:
-        key = tuple(getattr(self.context, level) for level in self.level)
-        return self.data.xs(key=key, level=self.level)[column]
+        levels = self.level
+        key = tuple(getattr(self.context, level) for level in levels)
+
+        if key[-1] is None:
+            levels = self.level[:-1]
+            key = key[:-1]
+        return self.data.xs(key=key, level=levels)[column]
 
     def group_by(self, level: list[str, ...], operation) -> pd.DataFrame:
         groupped = self.data.groupby(level=level)
@@ -40,33 +46,38 @@ class AnalysisSiti(AnalysisBase):
         self.projection = 'cmp'
         self.keys = ['name', 'projection', 'tiling', 'tile', 'quality']
         self.tiling_list = self.tiling_list[1:]
+
         self.siti = SitiData(f'dataset/siti.pickle', self)
 
     def make_stats(self):
-        for self.name in self.name_list:
-            for self.tile in self.tile_list:
-                si = pd.Series(self.siti['si'].iloc[0])
-                ti = pd.Series(self.siti['ti'].iloc[0])
-                self.stats_defaultdict['Metric'].append(self.name)
-                self.stats_defaultdict['Tiling'].append(self.tiling)
-                self.stats_defaultdict['Quality'].append(self.quality)
-                self.stats_defaultdict['Tile'].append(self.tile)
+        for self.group, video_list in self.video_list_by_group.items():
+            for self.name in video_list:
+                for self.tile in self.tile_list:
+                    si = self.siti['si']
+                    ti = self.siti['ti']
 
-                self.stats_defaultdict['si_Média'].append(si.mean())
-                self.stats_defaultdict['si_Desvio Padrão'].append(si.std())
-                self.stats_defaultdict['si_Mínimo'].append(si.quantile(0.00))
-                self.stats_defaultdict['si_1º Quartil'].append(si.quantile(0.25))
-                self.stats_defaultdict['si_Mediana'].append(si.quantile(0.50))
-                self.stats_defaultdict['si_3º Quartil'].append(si.quantile(0.75))
-                self.stats_defaultdict['si_Máximo'].append(si.quantile(1.00))
+                    self.stats_defaultdict['Group'].append(self.group)
+                    self.stats_defaultdict['Name'].append(self.name)
+                    self.stats_defaultdict['Tiling'].append(self.tiling)
+                    self.stats_defaultdict['Quality'].append(self.quality)
+                    self.stats_defaultdict['Tile'].append(self.tile)
 
-                self.stats_defaultdict['ti_Media'].append(ti.mean())
-                self.stats_defaultdict['ti_Desvio Padrão'].append(ti.std())
-                self.stats_defaultdict['ti_Mínimo'].append(ti.quantile(0.00))
-                self.stats_defaultdict['ti_1º Quartil'].append(ti.quantile(0.25))
-                self.stats_defaultdict['ti_Mediana'].append(ti.quantile(0.50))
-                self.stats_defaultdict['ti_3º Quartil'].append(ti.quantile(0.75))
-                self.stats_defaultdict['ti_Máximo'].append(ti.quantile(1.00))
+                    self.stats_defaultdict['si_Média'].append(si.mean())
+                    self.stats_defaultdict['si_Desvio Padrão'].append(si.std())
+                    self.stats_defaultdict['si_Mínimo'].append(si.quantile(0.00))
+                    self.stats_defaultdict['si_1º Quartil'].append(si.quantile(0.25))
+                    self.stats_defaultdict['si_Mediana'].append(si.quantile(0.50))
+                    self.stats_defaultdict['si_3º Quartil'].append(si.quantile(0.75))
+                    self.stats_defaultdict['si_Máximo'].append(si.quantile(1.00))
+
+                    self.stats_defaultdict['ti_Media'].append(ti.mean())
+                    self.stats_defaultdict['ti_Desvio Padrão'].append(ti.std())
+                    self.stats_defaultdict['ti_Mínimo'].append(ti.quantile(0.00))
+                    self.stats_defaultdict['ti_1º Quartil'].append(ti.quantile(0.25))
+                    self.stats_defaultdict['ti_Mediana'].append(ti.quantile(0.50))
+                    self.stats_defaultdict['ti_3º Quartil'].append(ti.quantile(0.75))
+                    self.stats_defaultdict['ti_Máximo'].append(ti.quantile(1.00))
+        self.save_stats()
 
     def plots(self):
         self.plot1()
