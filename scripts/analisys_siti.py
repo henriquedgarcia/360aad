cc020f8fdad1147ed2ebaeb65f53322e86f4b437
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-04-20 17:41:17 -0300

    Update analisys_siti.py

--- scripts/analisys_siti.py
+++ scripts/analisys_siti.py
@@ -1,8 +1,8 @@
 import os
 from collections import defaultdict
 from pathlib import Path
+from typing import Union
 
-import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
@@ -20,7 +20,13 @@ class SitiData:
         self.context = context
         self.data: pd.DataFrame = load_pd_pickle(Path(filename))
 
-    def __getitem__(self, column) -> pd.Series:
+    def __getitem__(self, column) -> Union[int, float]:
+        """
+        if str, search colum, full index
+        if tuple, search index using cross-section
+        :param column:
+        :return:
+        """
         levels = self.level
         key = tuple(getattr(self.context, level) for level in levels)
 
@@ -28,6 +34,10 @@ class SitiData:
             levels = self.level[:-1]
             key = key[:-1]
         return self.data.xs(key=key, level=levels)[column]
+
+    def xs(self, levels):
+        key = tuple(getattr(self.context, level) for level in levels)
+        return self.data.xs(key=key, level=levels)
 
     def group_by(self, level: list[str], operation) -> pd.DataFrame:
         grouped = self.data.groupby(level=level)
@@ -39,66 +49,50 @@ def frame_to_chunk(frame_serie):
     for i in range(60):
         idx = i * 30
         filtered.append(frame_serie[idx:idx + 30].mean())
-    return filtered
+    return pd.Series(filtered, name='frame_to_chunk')
 
 
 class AnalysisSiti(AnalysisBase):
     siti: SitiData
-    keys: list
+    tile_map: dict
 
     def setup(self):
         self.stats_defaultdict = defaultdict(list)
         self.projection = 'cmp'
         self.tiling = '3x2'
         self.quality = 28
-        self.projection = 'cmp'
-        self.keys = ['name', 'projection', 'tiling', 'tile', 'quality']
-        self.tiling_list = self.tiling_list[1:]
 
+        self.tile_map = {0: 'left', 1: 'front', 2: 'right', 3: 'down', 4: 'back', 5: 'top'}
         self.siti = SitiData(f'dataset/siti.pickle', self)
 
     def make_stats(self):
-        if self.stats_csv.exists(): return
+        # if self.stats_csv.exists(): return
 
         for self.group, video_list in self.video_list_by_group.items():
             for self.name in video_list:
-                for self.tile in self.tile_list:
-                    si = self.siti['si']
-                    ti = self.siti['ti']
+                data = self.siti.xs(('name',))
+                data_mean = data.groupby(['tile']).mean().mean()
+                data_std = data.groupby(['tile']).std().mean()
 
-                    self.stats_defaultdict['Group'].append(self.group)
-                    self.stats_defaultdict['Name'].append(self.name)
-                    self.stats_defaultdict['Tiling'].append(self.tiling)
-                    self.stats_defaultdict['Quality'].append(self.quality)
-                    self.stats_defaultdict['Tile'].append(self.tile)
+                self.stats_defaultdict['Group'].append(self.group)
+                self.stats_defaultdict['Name'].append(self.name)
+                self.stats_defaultdict['Tiling'].append(self.tiling)
+                self.stats_defaultdict['Quality'].append(self.quality)
 
-                    self.stats_defaultdict['si_Média'].append(si.mean())
-                    self.stats_defaultdict['si_Desvio Padrão'].append(si.std())
-                    self.stats_defaultdict['si_Mínimo'].append(si.quantile(0.00))
-                    self.stats_defaultdict['si_1º Quartil'].append(si.quantile(0.25))
-                    self.stats_defaultdict['si_Mediana'].append(si.quantile(0.50))
-                    self.stats_defaultdict['si_3º Quartil'].append(si.quantile(0.75))
-                    self.stats_defaultdict['si_Máximo'].append(si.quantile(1.00))
-
-                    self.stats_defaultdict['ti_Media'].append(ti.mean())
-                    self.stats_defaultdict['ti_Desvio Padrão'].append(ti.std())
-                    self.stats_defaultdict['ti_Mínimo'].append(ti.quantile(0.00))
-                    self.stats_defaultdict['ti_1º Quartil'].append(ti.quantile(0.25))
-                    self.stats_defaultdict['ti_Mediana'].append(ti.quantile(0.50))
-                    self.stats_defaultdict['ti_3º Quartil'].append(ti.quantile(0.75))
-                    self.stats_defaultdict['ti_Máximo'].append(ti.quantile(1.00))
-
+                self.stats_defaultdict['si_mean'].append(data_mean['si'])
+                self.stats_defaultdict['si_std_mean'].append(data_std['si'])
+                self.stats_defaultdict['ti_Media'].append(data_mean['ti'])
+                self.stats_defaultdict['ti_std_mean'].append(data_std['ti'])
         self.save_stats()
 
     def plots(self):
-        # self.plot1()
-        # self.plot2()
-        # self.boxplot1()
+        # self.plot_by_frame()
+        # self.plot_by_chunk()
+        # self.boxplot_mean()
         # self.boxplot2()
         self.scatter_mean_x_std()
 
-    def plot1(self):
-        tile_map = {0: 'left', 1: 'front', 2: 'right', 3: 'down', 4: 'back', 5: 'top'}
+    def plot_by_frame(self):
 
         def plot_path():
             filename = self.graphs_workfolder / 'plot_tiles' / f'plot_{self.group}_{self.name}_by_frame.png'
@@ -115,8 +109,8 @@ class AnalysisSiti(AnalysisBase):
                     si = self.siti['si']
                     ti = self.siti['ti']
 
-                    ax1.plot(si, label=f'{tile_map[self.tile]}')
-                    ax2.plot(ti, label=f'{tile_map[self.tile]}')
+                    ax1.plot(si, label=f'{self.tile_map[self.tile]}')
+                    ax2.plot(ti, label=f'{self.tile_map[self.tile]}')
                 ax1.legend()
                 ax1.set_title(f'SI')
                 ax1.set_xlabel(f'Frame')
@@ -127,7 +121,7 @@ class AnalysisSiti(AnalysisBase):
                 fig.savefig(plot_path())
                 plt.close()
 
-    def plot2(self):
+    def plot_by_chunk(self):
         tile_map = {0: 'left', 1: 'front', 2: 'right', 3: 'down', 4: 'back', 5: 'top'}
 
         def plot_path():
@@ -157,34 +151,29 @@ class AnalysisSiti(AnalysisBase):
                 fig.savefig(plot_path())
                 plt.close()
 
-    def boxplot1(self):
-        def plot_path(m):
-            filename = self.graphs_workfolder / 'boxplot1' / f'bar_{m}_mean.png'
+    def boxplot_mean(self):
+        def plot_path(name):
+            filename = self.graphs_workfolder / 'boxplot_tile' / f'boxplot_tile_{name}.png'
             filename.parent.mkdir(parents=True, exist_ok=True)
             return filename
 
-        data = self.siti.group_by(['name', 'tile'], 'mean')
-        ticks_position = list(range(1, len(self.name_list) + 1))
-
-        for metric in ['si', 'ti']:
-            sorted_names = data.groupby('name').mean()[metric].sort_values().reset_index()['name'].to_list()
-            metric_list = [data.loc[(self.name,)][metric] for self.name in sorted_names]
-            legends_list = [f'{pos}-{name}' for pos, name in zip(ticks_position, sorted_names)]
-
+        data = self.siti.group_by(['name', 'tile', 'frame'], 'mean')
+        ticks_position = list(range(1, len(self.tile_list) + 1))
+        ticks_names = [self.tile_map[tile] for tile in self.tile_list]
+        for self.name in self.name_list:
             fig = plt.figure(figsize=(14, 6), dpi=300, layout="constrained")
-            ax1 = fig.add_subplot(1, 1, 1)
-            ax1.boxplot(metric_list, widths=0.8)
-            ax1.set_title(metric.upper())
-            ax1.set_xticklabels(ticks_position)
+            for idx, metric in enumerate(['si', 'ti']):
+                metric_list = [data.xs((self.name, self.tile))[metric] for self.tile in self.tile_list]
 
-            legend = fig.legend(legends_list, loc='outside right center', handlelength=0, handletextpad=0)
-            for handler in legend.legend_handles:
-                handler.set_visible(False)
-
-            fig.savefig(plot_path(metric))
+                ax: plt.Axes = fig.add_subplot(2, 1, idx + 1)
+                ax.boxplot(metric_list, widths=0.8)
+                ax.set_title(metric.upper())
+                ax.set_xticks(ticks_position, ticks_names)
+            plt.show()
+            fig.savefig(plot_path(self.name))
             plt.close()
 
-    def boxplot2(self):
+    def boxplot_std(self):
         def plot_path(m):
             filename = self.graphs_workfolder / 'boxplot1' / f'bar_{m}_std.png'
             filename.parent.mkdir(parents=True, exist_ok=True)
@@ -217,7 +206,7 @@ class AnalysisSiti(AnalysisBase):
             filename.parent.mkdir(parents=True, exist_ok=True)
             return filename
 
-        data:pd.DataFrame = self.siti.group_by(['name', 'tile'], 'mean')
+        data: pd.DataFrame = self.siti.group_by(['name', 'tile'], 'mean')
         data_mean = data.groupby(['name']).mean()
         data_std = data.groupby(['name']).std()
         fig = plt.figure(figsize=(14, 7), dpi=300, layout="constrained")
