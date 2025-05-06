6feb0eb74d2f6dbca077886cfa61a2ddf85277de
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-05-06 18:25:01 -0300

    Update fix_database.py

--- scripts/fix_database.py
+++ scripts/fix_database.py
@@ -11,39 +11,20 @@ from scripts.utils.utils import load_json, load_pd_pickle
 
 
 class FixDatabase(AnalysisPaths):
-
     @property
     def database_json(self):
         database_path = Path(f'dataset/{self.metric}')
         return database_path / f'{self.metric}_{self.name}.json'
 
-    def __init__(self, config):
+    def __init__(self, config: Config):
         print(f'{self.__class__.__name__} initializing...')
         self.config = config
         self.projection = 'cmp'
+
         self.fix()
-        self.dataset_structure = {
-            'siti': {'get_tiles_chunk': ['name', 'projection', 'tiling', 'tile', 'value'],
-                     },
-            'get_tiles': {'get_tiles_chunk': ['name', 'projection', 'tiling', 'tile', 'category', 'value', None, None],
-                          'get_tiles_frame': ['name', 'projection', 'tiling', 'tile', 'category', 'value', None, None],
-                          },
-            'bitrate': {'dash_mpd': ['name', 'projection', 'tiling', 'tile', 'category', 'value', None, None],
-                        'dash_init': ['name', 'projection', 'tiling', 'tile', 'quality', 'category', 'value', None],
-                        'dash_m4s': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'value']
-                        },
-            'time': {'dectime_avg': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'value'],
-                     'dectime_std': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'value'],
-                     },
-            'chunk_quality': {'ssim': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'frame', 'value'],
-                              'mse': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'frame', 'value'],
-                              's-mse': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'frame', 'value'],
-                              'ws-mse': ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk', 'category', 'frame', 'value'],
-                              }
-            }
 
     def fix(self):
-        self.fix_siti()
+        # self.fix_siti()
 
         # self.fix_head_movement()
 
@@ -51,60 +32,78 @@ class FixDatabase(AnalysisPaths):
 
         # self.fix_viewport_quality()
 
-        # self.fix_bitrate()
+        self.fix_bitrate()
         # self.fix_dectime()
         # self.fix_chunk_quality()
         # self.join_metrics()
 
-    def fix_siti(self):
-        metric = 'siti'
-        self.dataset_structure[metric] = {'keys':
-                                              ['name', 'projection', 'tiling', 'tile', 'quality', 'si', 'ti'],
-                                          'path': Path(f'dataset/{metric}.pickle')
-                                          }
-        new_database = Path(f'dataset/{metric}.pickle')
-
-        # if new_database.exists():
-        #     return
-
-        columns = list(self.dataset_structure[metric]['keys'])
-
-        for self.name in ['wingsuit_dubai']:
-            print(self.name)
-            data_full = load_pd_pickle(Path(f'dataset/{metric}/{metric}_{self.name}.pickle'))
-
-        print('\nSaving')
-        data_full.sort_index(inplace=True)
-        data_full.to_pickle(new_database)
-
-    def fix_viewport_quality(self):
-        pass
-
     def fix_bitrate(self):
-        new_database = Path(f'dataset/bitrate.pickle')
-        if new_database.exists():
-            return
-
         metric = 'bitrate'
+        new_database = self.dataset_structure[metric]['path']
+
         bitrate_data = []
+        mpd_data = []
 
-        for name in self.name_list:
-            database = load_json(Path(f'dataset/{metric}/{metric}_{name}.json'))
-
-            for projection in self.projection_list:
-                for tiling in self.tiling_list:
-                    for tile in self.tiling_list[tiling]:
-                        for quality in self.quality_list:
-                            print(f'\r({metric}_{name}_{projection}_{tiling}_tile{tile}_qp{quality})', end='')
-                            for chunk in self.chunk_list:
-                                data = database[name][projection][tiling][tile][quality][chunk]['dash_m4s']
-                                bitrate_data.append((name, projection, tiling, int(tile), int(quality), int(chunk), int(data)))
+        for self.name in self.name_list:
+            database = load_json(Path(f'dataset/{metric}/{metric}_{self.name}.json'))
+            for self.tiling in self.tiling_list:
+                for self.tile in self.tile_list:
+                    database[self.name][self.projection][self.tiling][str(self.tile)]['dash_mpd']
+                    mpd_data.append()
+                    for self.quality in self.quality_list:
+                        print(f'\r({metric}_{self.name}_{self.projection}_{self.tiling}_tile{self.tile}_qp{self.quality})', end='')
+                        for self.chunk in self.chunk_list:
+                            data = database[self.name][self.projection][self.tiling][str(self.tile)][str(self.quality)][str(self.chunk+1)]['dash_m4s']
+                            bitrate_data.append((self.name, self.projection, self.tiling, self.tile, self.quality, self.chunk, int(data)))
         print('\nSaving')
         columns = ['name', 'projection', 'tiling', 'tile', 'quality', 'chunk']
         df = pd.DataFrame(bitrate_data, columns=columns + [metric])
         df = df.set_index(columns)
         serie = df[metric]
         serie.to_pickle(new_database)
+
+    def fix_siti(self):
+        metric = 'siti'
+        self.name = 'wingsuit_dubai'  # um erro fez todos os valores ficarem no último arquivo
+
+        data_full: pd.DataFrame = load_pd_pickle(Path(f'dataset/{metric}/{metric}_{self.name}.pickle'))
+        index_names = list(data_full.index.names) + ['frame']
+        df_list = []
+        for index, series in data_full.iterrows():
+            index: tuple
+            index_list = [index + (i,) for i in range(1800)]
+            multi_index = pd.MultiIndex.from_tuples(index_list, names=index_names)
+            df = pd.DataFrame({'si': series[0], 'ti': series[1]}, index=multi_index)
+            df_list.append(df)
+        df_final = pd.concat(df_list)
+        df_final.sort_index(inplace=True)
+
+        print('\nSaving')
+        new_database = self.dataset_structure['siti']['path']
+        df_final.to_pickle(new_database)
+
+    def fix_head_movement(self):
+        new_database = self.dataset_structure['head_movement']['path']
+
+        head_movement_by_frame = []
+        for self.name in self.name_list:
+            for self.user in self.users_by_name:
+                print(f'\r{self.name} {self.projection} user{self.user}', end='')
+
+                head_movement = self.config.hmd_dataset[self.name + '_nas'][self.user]
+                for self.frame, (yaw, pitch, roll) in enumerate(head_movement):
+                    register = (self.name, self.projection, int(self.user), self.frame,
+                                yaw, pitch, roll)
+                    head_movement_by_frame.append(register)
+
+        index = ['name', 'projection', 'user', 'frame']
+        df = pd.DataFrame(head_movement_by_frame, columns=index + ['yaw', 'pitch', 'roll'])
+        df = df.set_index(index)
+        df.to_pickle(new_database)
+
+    def fix_viewport_quality(self):
+        pass
+
 
     def fix_dectime(self):
         new_database = Path(f'dataset/dectime.pickle')
@@ -193,26 +192,6 @@ class FixDatabase(AnalysisPaths):
         serie = df['seen_tiles']
         serie.to_pickle(new_database)
 
-    def fix_head_movement(self):
-        new_database = Path(f'dataset/head_movement.pickle')
-        # if new_database.exists(): return
-
-        head_movement_by_frame = []
-        for self.name in self.name_list:
-            for self.user in self.users_by_name:
-                print(f'\r{self.name} {self.projection} user{self.user}', end='')
-
-                head_movement = self.config.hmd_dataset[self.name + '_nas'][self.user]
-                for self.frame, (yaw, pitch, roll) in enumerate(head_movement):
-                    register = (self.name, self.projection, int(self.user), self.frame,
-                                yaw, pitch, roll)
-                    head_movement_by_frame.append(register)
-
-        index = ['name', 'projection', 'user', 'frame']
-        df = pd.DataFrame(head_movement_by_frame, columns=index + ['yaw', 'pitch', 'roll'])
-        df = df.set_index(index)
-        df.to_pickle(new_database)
-
     def join_metrics(self):
         final_name = lambda: Path(f'dataset/metrics.pickle')
         metric_name = lambda: Path(f'dataset/pickles/0_grouped_{self.metric}.pickle')
@@ -271,14 +250,9 @@ class FixDatabase(AnalysisPaths):
     def update_ui(self, desc: str):
         self.ui.update(desc)
 
-    _dataset_structure = {}
     @property
     def dataset_structure(self):
-        return self._dataset_structure
-
-    @dataset_structure.setter
-    def dataset_structure(self, value):
-        self._dataset_structure = value
+        return self.config.dataset_structure
 
 
 if __name__ == '__main__':
