35970b01158498468421d7fada449169d4852d84
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-08-17 18:39:51 -0300

    Update utils.py

--- scripts/utils/utils.py
+++ scripts/utils/utils.py
@@ -313,13 +313,50 @@ def ea2xyz(ea: np.ndarray) -> np.ndarray:
     return xyz
 
 
+class Bcolors:
+    CYAN = '\033[96m'
+    PINK = '\033[95m'
+    BLUE = '\033[94m'
+    YELLOW = '\033[93m'
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    GRAY = '\033[90m'
+
+    BG_GRAY = '\033[47m'
+    BG_CYAN = '\033[46m'
+    BG_PINK = '\033[45m'
+    BG_BLUE = '\033[44m'
+    BG_YELLOW = '\033[43m'
+    BG_GREEN = '\033[42m'
+    BG_RED = '\033[41m'
+    BG_BLACK = '\033[40m'
+
+    INVERT = '\033[6m'
+    BLINK = '\033[5m'
+    UNDERLINE = '\033[4m'
+    ITALIC = '\033[3m'
+    WEAK = '\033[2m'
+    BOLD = '\033[1m'
+
+    CLEAR = '\033[A'
+
+    ENDC = '\033[0m'
+
+
+class AbortError(Exception): ...
+
+
+def print_error(msg: str, end: str = '\n'):
+    print(f'{Bcolors.RED}{msg}{Bcolors.ENDC}', end=end)
+
+
 @contextmanager
 def task(self, verbose=True):
     if verbose:
         print(f'\r==== {self.__class__.__name__} {self.ctx} ====', end='')
     try:
         yield
-    except AbortError as e:
+    except (AbortError, AssertionError) as e:
         msg = e.args[0]
         if msg and verbose:
             print_error(f'\t{e.args[0]}')
